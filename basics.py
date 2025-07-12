3e24ec906ec85131fe2e82d0a00c90c5aa8a1917
san <113934667+SaniyaAg@users.noreply.github.com>
2025-07-12 18:11:23 +0000

    add python lists and dictionary

---
+++ basics.py
@@ -0,0 +1,20 @@
+print("***arrays")
+
+a = [1,2,3,4]
+
+print(a)
+
+
+print("***loops")
+for i in [1, 10, 25, 13]:
+    print(2*i*i)
+
+for j in ["maths", "phy", "sci"]:
+    print(j)    
+
+print("***dict")
+s = {"name": "raj", 'maths' : 98, 'hobby' :"running"}
+print(s)
+
+for i, j in s.items():
+    print(i,j)
